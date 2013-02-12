5299ece09c46370701d2acf4e510ab82f0b3578d
benortiz <benortiz@ocf.berkeley.edu>
2013-02-11 21:15:45 -0800

    message

--- calfood/templates/static/resources/js/calfoodalert.js
+++ calfood/templates/static/resources/js/calfoodalert.js
@@ -1,8 +1,12 @@
 function get(el) {
+	console.log("getting");
 	return document.getElementById(el);
 }
 
 function addFavorite(fav) {
 	var chooser = get(chosen_favorites);
+	console.log("chooser");
 	chooser.innerHTML = "<li>" + fav + "</li>";
+	console.log(fav + "what happened");
+	return false;
 }
