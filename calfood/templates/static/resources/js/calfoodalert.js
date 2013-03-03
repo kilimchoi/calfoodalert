d187a3fc5147168d5011b2ade2f4dd9878c22797
benortiz <benortiz@ocf.berkeley.edu>
2013-03-03 05:30:21 -0800

    some changes that might add a small bit of complexity, the phone field is now 3 fields to help prevent users from formatting the number, all we have to do is add the three together to get the full phone.

--- calfood/templates/static/resources/js/calfoodalert.js
+++ calfood/templates/static/resources/js/calfoodalert.js
@@ -13,20 +13,62 @@ $(document).ready(function() {
 		$('#video_cover').hide();
 	});
 	$('.register').click(function() {
-		var $tele = $('#register .tele').val();
 		var $pwd = $('#register .pwd').val();
 		var $pwd_conf = $('#register .pwd_conf').val();
-		if ($tele.length < 10) {
-			$('#register .error').replaceWith("<p class=\"error\">please enter a 10 digit phone number with only numbers</p>");
-		}
-		else if ($pwd != $pwd_conf) {
+		if ($pwd != $pwd_conf) {
 			$('#register .error').replaceWith("<p class=\"error\">passwords do not match</p>");
+			return false;
 		} 
 		else if ($pwd.length < 6) {
 			$('#register .error').replaceWith("<p class=\"error\">length</p>");
+			return false;
 		}
 		else {
 			location.href = "index#verify";	
+		}
+	});
+	$('.verify').click(function() {
+		var $code = $('#verify .code').val();
+		if ($code.length < 6) {
+			$('#verify .error').replaceWith("<p class=\"error\">length</p>");
+			return false;
+		}
+		else if ($code.type() != "number" && Math.floor($code) != $code) {
+			$('#verify .error').replaceWith("<p class=\"error\">length</p>");
+			return false;
+		}
+		else {
+			location.href = "favorites";
+		}
+	});
+	$('#register .phone.area').keyup(function(){
+		if($(this).val().length>=$(this)[0].maxLength){
+			$('#register .phone.first').focus();
+		}
+	});
+	$('#login .phone.area').keyup(function(){
+		if($(this).val().length>=$(this)[0].maxLength){
+			$('#login .phone.first').focus();
+		}
+	});
+	$('#register .phone.first').keyup(function(){
+		if($(this).val().length>=$(this)[0].maxLength){
+			$('#register .phone.last').focus();
+		}
+	});
+	$('#login .phone.first').keyup(function(){
+		if($(this).val().length>=$(this)[0].maxLength){
+			$('#login .phone.last').focus();
+		}
+	});	
+	$('#register .phone.last').keyup(function(){
+		if($(this).val().length>=$(this)[0].maxLength){
+			$('#register .pwd').focus();
+		}
+	});
+	$('#login .phone.last').keyup(function(){
+		if($(this).val().length>=$(this)[0].maxLength){
+			$('#login .pwd').focus();
 		}
 	});
 });
@@ -35,7 +77,7 @@ function adjustSize() {
 	var width = $(window).width() - .3 * $(window).width();
 	var videoHeight = width/2.39;
 	var footerWidth = $(window).width();
-	$('#landing .video, body, header, #landing, #favorites, footer > div').width(width);
+	$('#landing .video, body, header, #landing, #favorites, footer > div, #favorites #fav_search').width(width);
 	$('#landing .video').height(videoHeight);	
 	$('footer').width(footerWidth);
 	$('#landing .left_column, #landing .right_column').width(width/2 - 10);
